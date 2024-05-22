747eaffa8756384169958a28d182d441c21a34b0
Kimberly <ae.xerx@gmail.com>
2024-05-22 23:21:55 +0800

    constructor

--- tv.py
+++ tv.py
@@ -1,11 +1,6 @@
 #  Constructor
 class TV:
-    def __int__(self):
-        self.channel = 1
-        self.volume_level = 1
-        self.on = False
-
-    #  Method to turn on the TV
-    def turn_on(self):
-        self.on = True
+    def __init__(self):
         
+        self.on = None
+
