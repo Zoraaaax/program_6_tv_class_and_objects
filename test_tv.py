a3bfcb833c48ecd7ff2a735964ebee55a5a478fb
Kimberly <ae.xerx@gmail.com>
2024-05-23 00:20:10 +0800

    pseudocode

--- test_tv.py
+++ test_tv.py
@@ -1,10 +1,1 @@
-#  Import the TV class from the tv module.
-from tv import TV
-
-
-#  Define a TestTV class
-class TestTV:
-
-    #  Define a method test_tv
-    def test_tv(self):
-        
+#  Import the TV class from the tv module
